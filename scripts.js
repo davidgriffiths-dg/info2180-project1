4687d360e85f4ba2b4444453e84015bfda0f263f
David <griffithsdavidchris@gmail.com>
2020-11-01 23:07:31 -0500

    Added script functionality

--- scripts.js
+++ scripts.js
@@ -1,1 +1,31 @@
 /* Add your JavaScript to this file */
+
+    window.onload = function () {
+
+        const subscribe = document.querySelectorAll('.btn');
+        
+       subscribe[1].addEventListener('click', function(event) {
+           event.preventDefault();
+           var  container = document.getElementsByClassName("message");
+           if (container[0].innerHTML!=""){
+               container[0].removeChild(container[0].lastElementChild);  
+           }
+           var email= document.getElementById("email").value;
+           var x = document.createElement("x") ;
+           var message="";
+           var mail= " has been added to our mailing list!";
+           if(email!="")
+           {
+                 message = document.createTextNode('Thank You! Your email address '.concat(email,mail));
+      
+            }else
+
+            {
+             message = document.createTextNode('Please enter a valid email address');
+            }
+            x.appendChild(message)
+           container[0].appendChild(x);
+      })
+   
+   
+      }
